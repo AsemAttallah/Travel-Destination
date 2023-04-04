053f2d574f8fe30c0201a8c6b8f614ff12fd5f88
AsemAttallah <attallahasem@gmail.com>
2023-04-04 20:26:10 +0000

    a

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import './App.css';
+import { Routes, Route } from "react-router-dom";
 import Home from './components/home/Home';
+import TourDetails from './components/TourDetails/TourDetails';
 const toursData = require('./data/db.json');
-
 
 function App() {
   return (
+    <>
     <div class="all"> 
-    <Home data={toursData}/>
     </div>
+    <Routes>
+      <Route path="/" element={<Home data={toursData} />} />
+      <Route path="/city/:id" element={<TourDetails />} />
+    </Routes>
+    </>
   );
 }
 
---
+++ src/components/TourDetails/TourDetails.js
@@ -0,0 +1,7 @@
+function TourDetails (){
+    return(
+        <h1 class="detail">more details</h1>
+    )
+}
+
+export default TourDetails;
